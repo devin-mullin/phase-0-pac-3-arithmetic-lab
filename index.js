32267d3bda201ff9aa089f6e63e920971ad05046
Devin Mullin <devin.mullin90@gmail.com>
2021-08-13 17:21:24 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function increment(n) {
+    return ++n
+}
+
+function decrement(n) {
+    return --n
+}
+
+parseInt (22.22, 10)
+
+parseFloat (80.123999)
+
+console.log(increment(3))
+
+console.log(decrement(3))
+
+console.log(add(1, 2))
+
+console.log(subtract(2, 1))
+
+console.log(multiply(3,2))
+
+console.log(divide(8,4))
+
